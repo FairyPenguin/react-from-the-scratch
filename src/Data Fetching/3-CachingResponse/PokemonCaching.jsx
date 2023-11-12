3198bc57429005678badd8d2046a765fc4d5a939
FairyPenguin <autorun81@gmail.com>
2023-11-12 12:05:58 +0200

    Data Fetching

--- src/Data Fetching/3-CachingResponse/PokemonCaching.jsx
+++ src/Data Fetching/3-CachingResponse/PokemonCaching.jsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+
 
 function PokemonCaching() {
+    //States
     const [input, setInput] = useState("")
     const [inputPokemon, setInputPokemon] = useState("")
     const [pokemon, setPokemon] = useState({
@@ -8,66 +10,104 @@ function PokemonCaching() {
         weight: "",
         image: ""
     })
-
     const [errorMessage, setErrorMessage] = useState(false)
 
+    //Cache Map
 
+    const PokemonCacheRef = useRef(new Map())
 
+    //URL 
     const URL =
-        `https://pokeapi.co/api/v2/pokemon/${input}`
 
-    useEffect(
-        () => {
-
-            const fetchHandler = async () => {
-                try {
-                    const response = await fetch(URL)
-                    const Data = await response.json()
-                    // if (!response.ok) {
-                    //     console.log("Error...Fetching Failed")
-                    //     setErrorMessage(true)
-                    // }
-
-                    const pokemonName = Data.name
-                    const pokemonWeight = Data.weight
-                    const pokemonImage = Data.sprites.other["official-artwork"].front_default
-
-                    setPokemon({
-                        name: pokemonName,
-                        weight: pokemonWeight,
-                        image: pokemonImage
-                    })
-
-
-
-                } catch (error) {
-                    // console.log(error)
-                    // setErrorMessage(true)
-
-                }
-
-            }
-            fetchHandler()
-        }, [URL])
-
-
-
-    function handelSubmit(e) {
-        e.preventDefault()
-        setInputPokemon(input)
-        console.log(inputPokemon)
-        console.log(URL)
-        setInput("")
-    }
-
+        `https://pokeapi.co/api/v2/pokemon/${inputPokemon}`
 
     function inputHandler(e) {
 
         let inputValue = e.target.value
 
         setInput(inputValue)
+
     }
 
+
+
+    function handelSubmit(e) {
+        e.preventDefault()
+        setInputPokemon(input)
+
+        // if (PokemonCacheRef.current.has(inputPokemon)) {
+        //     console.log("it's Here")
+        //     PokemonCacheRef.current.get(inputPokemon)
+
+        // }
+        setInput("")
+
+    }
+
+    //Effect
+    useEffect(
+        () => {
+
+            const fetchHandler = async () => {
+                try {
+
+                    if (inputPokemon === "") {
+                        return null
+                    }
+
+                    if (PokemonCacheRef.current.has(inputPokemon)) {
+                        console.log("IT's Here DUDE")
+                        setPokemon(PokemonCacheRef.current.get(inputPokemon))
+                        console.log(PokemonCacheRef.current.get(inputPokemon))
+                    } else {
+                        const response = await fetch(URL)
+                        const Data = await response.json()
+                        const pokemonName = Data.name
+                        const pokemonWeight = Data.weight
+                        const pokemonImage = Data.sprites.other["official-artwork"].front_default
+
+                        setPokemon({
+                            name: pokemonName,
+                            weight: pokemonWeight,
+                            image: pokemonImage
+                        })
+
+                        PokemonCacheRef.current.set(inputPokemon, {
+                            name: pokemonName,
+                            weight: pokemonWeight,
+                            image: pokemonImage
+                        })
+                    }
+
+
+
+
+
+
+                }
+
+                catch (error) {
+                    console.log(error)
+                    inputPokemon === "" ? setErrorMessage(false) : ""
+                    error ? setErrorMessage(true) : ""
+                }
+
+            }
+
+            fetchHandler()
+        }, [URL, inputPokemon])
+
+
+
+
+
+
+
+    // function Cache() {
+    //     setCache((prevState) => [...prevState, pokemon])
+
+
+    // }
 
 
 
@@ -83,7 +123,8 @@ function PokemonCaching() {
                 />
 
             </label>
-            <button style={{ display: "block", marginBottom: "1rem" }}
+            <button
+                style={{ color: "wheat", border: "5px solid wheat", width: "100%", display: "block", marginBottom: "1rem" }}
                 onClick={handelSubmit}
                 type='submit'
             >
@@ -92,12 +133,16 @@ function PokemonCaching() {
 
 
 
-            {errorMessage ? "Error from the API" :
+            {errorMessage ? "Check the correct pokemon name" :
 
                 <div  >
                     <h1>Name: {pokemon["name"]}</h1>
                     <img
-                        style={{ width: "200px", height: "200px" }} src={pokemon["image"]} alt="" />
+                        style={{ width: "200px", height: "200px", objectFit: "cover" }}
+                        src={pokemon["image"] === ""
+                            ? "https://www.pangea.global/wp-content/uploads/2022/08/Pokemon-Part-01_Picture-2.jpg"
+                            : pokemon["image"]}
+                        alt="" />
                     <p>Weight: {pokemon["weight"]}</p>
                 </div >}
         </>
