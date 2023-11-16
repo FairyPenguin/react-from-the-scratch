dc08e341537501a9fa906bd7c17cf792f0264132
FairyPenguin <autorun81@gmail.com>
2023-11-16 09:26:19 +0200

    Custom Hooks

--- src/Custom Hooks/exercise/4/Home4.jsx
+++ src/Custom Hooks/exercise/4/Home4.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react"
 import useQuery from "./useQuery"
 
 function Home4() {
+    const [pokemon, setPokemon] = useState("")
 
     // const fetchApi = useQuery()
+
+    const tickets = {
+        '0H2AZ123': null,
+        '23LA9T41': 'Verena Nardi',
+    };
+
+    function ticketStatus(tickets, ticketId) {
+
+        let TicketStatus = ""
+
+        if (tickets[ticketId] === null) {
+
+            TicketStatus = "not sold"
+
+        } else if (tickets[ticketId] === undefined) {
+            TicketStatus = "unknown ticket id"
+
+            // } else if (tickets[ticketId] !== undefined || tickets[ticketId] !== null) {
+        } else if (tickets[ticketId] ?? tickets[ticketId]) {
+
+            TicketStatus = `"sold to ${tickets[ticketId]}"`
+        }
+
+
+        return TicketStatus
+    }
+
+    console.log(ticketStatus(tickets, '23LA9T41'))
+
+    // console.log(ticketStatus(tickets, '0H2AZ123'))
+    // console.log(ticketStatus(tickets, 'RE90VAW7'))
+
+
 
     return (
         <div>Home4</div>
--- src/Custom Hooks/index.js
+++ src/Custom Hooks/index.js
@@ -5,3 +5,4 @@ export { default as Home2 } from "./exercise/2/Home2";
 export { default as Home3 } from "./exercise/3/Home3";
 
 export { default as Home4 } from "./exercise/4/Home4";
+
